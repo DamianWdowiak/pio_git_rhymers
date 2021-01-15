65b44cc8b5559600142d54a1996acfaca19a25b5
Damian Wdowiak <damian.wdowiak@outlook.com>
2021-01-15 18:02:21 +0100

    1.5 tests, documentation

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,11 +1,17 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Linked List that contains integers
+ */
 public class IntLinkedList {
 
     private Node last;
     private int i;
     public static final int IF_EMPTY = -1;
 
+    /**
+     * @param i new element to add
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -16,20 +22,32 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * @return true if list is empty
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * @return false
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * @return last value without deleting
+     */
     public int top() {
         if (isEmpty())
             return IF_EMPTY;
         return last.getValue();
     }
 
+    /**
+     * @return last value and delete it
+     */
     public int pop() {
         if (isEmpty())
             return IF_EMPTY;
@@ -38,4 +56,34 @@ public class IntLinkedList {
         return ret;
     }
 
+    private static class Node {
+
+        private final int VALUE;
+        private Node prev;
+        private Node next;
+
+        public Node(int i) {
+            this.VALUE = i;
+        }
+
+        public int getValue() {
+            return VALUE;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
